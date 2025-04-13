0c813799436748299d2040b33794c42364154f36
KarthikReddyPanyala <145713563+KarthikReddyPanyala@users.noreply.github.com>
2025-04-13 16:38:01 -0500

    Update evaluate_test.py

--- cholecseg_prepared/scripts/evaluate_test.py
+++ cholecseg_prepared/scripts/evaluate_test.py
@@ -1,1 +1,14 @@
-#put evaluation code here or thetesting code here
+#put evaluation code here or the testing code here
+from ultralytics import YOLO
+#imporrt model with best weight s
+model = YOLO("YOLO11/cholecseg_from_script3/weights/best.pt")
+
+metrics = model.val(
+    data="cholecseg_prepared/data.yaml",
+    split="test",
+    imgsz=640,
+    batch=8,
+    verbose=True
+)
+
+print(metrics)
